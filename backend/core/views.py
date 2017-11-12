7375acaa4bfcb1e5953e556fcc9ffb293d6be9da
litvaOo <evlen@qip.ru>
2017-11-12 20:49:10 +0200

    Changed landing template

--- backend/core/views.py
+++ backend/core/views.py
@@ -1,8 +1,14 @@
 from django.views.generic import TemplateView
+from jobs.models import Category
 
 
 class HomePageView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(HomePageView, self).get_context_data(**kwargs)
+        context['categories'] = Category.objects.all()[:6]
+        return context
 
 
 class LoginPageView(TemplateView):
