07d8d21bf99fd56e9029ac8e898ac928716836f6
Max Saykov <max-saykov@mail.ru>
2017-11-12 17:53:44 +0200

    Enable signup

--- backend/api/rest/serializers.py
+++ backend/api/rest/serializers.py
@@ -10,22 +10,37 @@ class UserSerializer(serializers.ModelSerializer):
 
 class SignupSerializer(serializers.Serializer):
     email = serializers.EmailField()
-    password1 = serializers.CharField(max_length=30)
-    password2 = serializers.CharField(max_length=30)
-    first_name = serializers.CharField(max_length=30)
-    last_name = serializers.CharField(max_length=30)
-    city = serializers.CharField(max_length=30)
-    country = serializers.CharField(max_length=30)
-    location = serializers.CharField(max_length=30)
+    password = serializers.CharField(max_length=255)
+    password2 = serializers.CharField(max_length=255)
+    first_name = serializers.CharField(max_length=255)
+    last_name = serializers.CharField(max_length=255)
+    city = serializers.CharField(max_length=255, required=False)
+    country = serializers.CharField(max_length=255, required=False)
+    location = serializers.CharField(max_length=255, required=False)
     avatar = serializers.FileField(required=False)
+
+    def validate_email(self, data):
+        if User.objects.filter(email=data).exists():
+            raise serializers.ValidationError(
+                "User with email {} exists".format(data))
+        return data
+
+    def validate_password(self, data):
+        if len(data) < 6:
+            raise serializers.ValidationError(
+                "Password length must be large then 6 chars")
+        if self.initial_data.get('password2') != data:
+            raise serializers.ValidationError(
+                "Passwords didn't match")
+        return data
 
     def save(self, request, **kwargs):
         return User.objects.create_user(
-            email=self.data['email'],
-            password=self.data['password1'],
-            first_name=self.data['first_name'],
-            last_name=self.data['last_name'],
-            city=self.data['city'],
-            country=self.data['country'],
-            avatar=self.data['avatar']
+            email=self.data.get('email'),
+            password=self.data.get('password'),
+            first_name=self.data.get('first_name'),
+            last_name=self.data.get('last_name'),
+            city=self.data.get('city'),
+            country=self.data.get('country'),
+            avatar=self.data.get('avatar')
         )
